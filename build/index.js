5c3179b918efbda1c860c6b31de7381862847a16
AntonPDC <antonpdecesare@gmail.com>
2026-09-21 11:25:27 -0400

    bubble sort

--- build/index.js
+++ build/index.js
@@ -1,6 +1,30 @@
 "use strict";
-console.log("Hi there");
-const logSomething = () => {
-    console.log("hi there");
-};
-logSomething();
+// console.log(123);
+// const logSomething = () => {
+//   console.log("hi there");
+// };
+// logSomething();
+// class Sorter {
+//   collection: number[];
+//   constructor(collection: number[]) {
+//     this.collection = collection;
+//   }
+// }
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([0, -1, 5, 10]);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
-console.log("Hi there");
+// console.log(123);
 
-const logSomething = () => {
-  console.log("hi there");
-};
+// const logSomething = () => {
+//   console.log("hi there");
+// };
 
-logSomething();
+// logSomething();
+
+// class Sorter {
+//   collection: number[];
+
+//   constructor(collection: number[]) {
+//     this.collection = collection;
+//   }
+// }
+
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+const sorter = new Sorter([0, -1, 5, 10]);
