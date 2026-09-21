544278ba95e13e577bb460c66c808c51c709aa13
AntonPDC <antonpdecesare@gmail.com>
2026-09-21 15:47:15 -0400

    commented out

--- build/index.js
+++ build/index.js
@@ -10,21 +10,19 @@
 //     this.collection = collection;
 //   }
 // }
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
-                }
-            }
-        }
-    }
-}
-const sorter = new Sorter([0, -1, 5, 10]);
+// class Sorter {
+//   constructor(public collection: number[]) {}
+//   sort(): void {
+//     const { length } = this.collection;
+//     for (let i = 0; i < length; i++) {
+//       for (let j = 0; j < length - i - 1; j++) {
+//         if (this.collection[j] > this.collection[j + 1]) {
+//           const leftHand = this.collection[j];
+//           this.collection[j] = this.collection[j + 1];
+//           this.collection[j + 1] = leftHand;
+//         }
+//       }
+//     }
+//   }
+// }
+// const sorter = new Sorter([0, -1, 5, 10]);
